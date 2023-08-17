cdfefcd267ab6bd9ec39a4c2d658fde9f37714b1
Arkadiusz Kordela <arkadiuszkordela@tutanota.com>
2023-08-17 22:02:54 +0200

    Update

--- ptg_project/settings.py
+++ ptg_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ['MY_DJANGO_KEY_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False   
+DEBUG = True   
 
-ALLOWED_HOSTS = [ptg.pythonanywhere.com]
+ALLOWED_HOSTS = []
 
 
 # Application definition
